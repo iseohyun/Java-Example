e9ddf5e97694baca788db4b4d4e644109e14d4c7
iseohyun <iseohyun@hanmail.net>
2021-08-14 01:24:01 +0900

    Argument Test

---
+++ D_Generic/src/D_Arguments/A_Arguments.java
@@ -0,0 +1,9 @@
+package D_Arguments;
+
+public class A_Arguments {
+	public static void main(String[] args) {
+		for(String str: args) {
+			System.out.println(str);
+		}
+	}
+}
---
+++ D_Generic/src/D_Arguments/B_Variables.java
@@ -0,0 +1,9 @@
+package D_Arguments;
+
+public class B_Variables {
+	public static void main(String [] args) {
+		for(String str: args) {
+			System.out.println(str);
+		}
+	}
+}
